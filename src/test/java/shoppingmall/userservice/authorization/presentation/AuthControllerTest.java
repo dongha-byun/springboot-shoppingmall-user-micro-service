7f1a2b6415227a8da57e89fd4181f142f839bfce
dongha-byun <byunsw4@naver.com>
2023-11-09 16:01:54 +0900

    refactor: Spring Security 제거

--- src/test/java/shoppingmall/userservice/authorization/presentation/AuthControllerTest.java
+++ src/test/java/shoppingmall/userservice/authorization/presentation/AuthControllerTest.java
@@ -6,7 +6,6 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,8 +22,6 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import shoppingmall.userservice.authorization.presentation.request.AuthRequest;
 
@@ -40,7 +37,6 @@ class AuthControllerTest {
     ObjectMapper objectMapper;
 
     @Test
-    @WithMockUser
     @DisplayName("사용자 정보를 통해 Access Token 을 발급받는다.")
     void auth() throws Exception {
         // given
@@ -48,13 +44,12 @@ class AuthControllerTest {
         Instant instant = currentLocalDateTime.atZone(ZoneId.systemDefault()).toInstant();
         Date currentDate = Date.from(instant);
         AuthRequest authRequest = new AuthRequest(
-                "test@test.com", "127.0.0.1", currentDate
+                100L, "127.0.0.1", currentDate
         );
         String requestBody = objectMapper.writeValueAsString(authRequest);
 
         // when & then
         mockMvc.perform(post("/auth")
-                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestBody))
                 .andDo(print())
@@ -62,7 +57,7 @@ class AuthControllerTest {
                 .andExpect(jsonPath("$.accessToken", notNullValue()))
                 .andDo(document("auth",
                         requestFields(
-                                fieldWithPath("email").description("사용자 가입 Email"),
+                                fieldWithPath("userId").description("사용자 고유 ID"),
                                 fieldWithPath("accessIp").description("접속 IP"),
                                 fieldWithPath("currentDate").description("토근 생성 일자")
                         ),
